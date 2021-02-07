6581d127b52c9ca1360a8cef445dc047af35cf8a
jgannod <jgannod@tntech.edu>
2021-02-07 17:54:31 -0600

    Added detailed instructions for the lab

--- test/TSharkDataTest.java
+++ test/TSharkDataTest.java
@@ -27,16 +27,12 @@ public class TSharkDataTest {
     public void getProtocol() {
         data = new TSharkData("\"8\",\"1.774700000\",\"TCP\",\"6\",\"192.168.68.126\",\"52.114.159.112\",\"55358\",\"443\",,");
         assertEquals("Protocol", "TCP", data.getProtocol());
-        data = new TSharkData("\"24\",\"1.330864000\",\"IGMPv3\",\"2\",\"192.168.68.1\",\"224.0.0.1\",,,,");
-        assertEquals("Protocol", "IGMPv3", data.getProtocol());
     }
 
     @Test
     public void getProtocolID() {
         data = new TSharkData("\"8\",\"1.774700000\",\"TCP\",\"6\",\"192.168.68.126\",\"52.114.159.112\",\"55358\",\"443\",,");
         assertEquals("Protocol ID", "6", data.getProtocolID());
-        data = new TSharkData("\"70\",\"7.373409000\",\"UDP\",\"17\",\"192.168.68.105\",\"192.168.68.255\",,,\"57621\",\"57621\"");
-        assertEquals("Protocol ID", "17", data.getProtocolID());
     }
 
     @Test
